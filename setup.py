f594c232fd8d952f8fc079f5221d8a5ded970342
yoelbassin <bassin.yoel@gmail.com>
2020-12-21 21:25:24 +0200

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='chat',
+    version='1',
+    packages=['client', 'server'],
+    url='',
+    license='',
+    author='bassi',
+    author_email='bassin.yoel@gmail.com',
+    description='Chat application'
+)
